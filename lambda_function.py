43f6859b510b827522224b48a94e8d885cc49e19
David <davidandres687@gmail.com>
2024-06-11 16:00:00 -0400

    lambda deployment 1

--- lambda_function.py
+++ lambda_function.py
@@ -237,6 +237,6 @@ def api_data_loader():
     df2 = df[['#','Date', 'City', 'Season', 'Industry / Event', 'Format', 'Attendance',  'Email', 'First Name', 'Last Name', 'Country of Origin', 'Area of Expertise', 'Employment Status', 'Employer', 'Dream Job', 'Linkedin']]
 
     set_with_dataframe(sheet_instance, df2, row=dfToUpdate.shape[0] + 2, include_column_header=False)
-    print('ok v2')
+    print('ok v3')
 
 #lambda_handler(1,2)
